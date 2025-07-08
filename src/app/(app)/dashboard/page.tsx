b23de7ebbf590e1396802e20c6bdafb11ec32fcc
Gabriel Castro <gabrielngc@gmail.com>
2025-07-07 21:38:12 -0300

    wip

---
+++ src/app/(app)/dashboard/page.tsx
@@ -0,0 +1,12 @@
+// app/page.tsx
+import { Button } from "@heroui/button";
+
+export default function DashboardPage() {
+  return (
+    <div>
+      <Button color="primary">Click me</Button>
+
+      <h2 className="text-green-700">Olá</h2>
+    </div>
+  );
+}
